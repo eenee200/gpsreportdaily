9345cf3be6db85640293d43e35b4494529716c60
eenee200 <162146991+eenee200@users.noreply.github.com>
2024-12-25 17:09:05 +0800

    Update gps_sensor_report.py

--- scripts/gps_sensor_report.py
+++ scripts/gps_sensor_report.py
@@ -16,7 +16,7 @@ CONFIG = {
     'GPS_DEVICE_ID': os.environ.get('GPS_DEVICE_ID'),
     'SENDER_EMAIL': os.environ.get('SENDER_EMAIL'),
     'SENDER_PASSWORD': os.environ.get('SENDER_PASSWORD'),
-    'RECEIVER_EMAILS': os.environ.get('RECEIVER_EMAILS').split(','),
+    'RECEIVER_EMAILS': os.environ.get('RECEIVER_EMAILS', '').split(',')
     'REPORT_FREQUENCY_DAYS': 1
 }
 
