b3902b4952a3e19d8abab4d7139896b76cf020f9
Enkh-Amidral Buyan-Ochir <162146991+eenee200@users.noreply.github.com>
2026-01-07 16:13:34 +0800

    Update gps_sensor_report.py

--- scripts/gps_sensor_report.py
+++ scripts/gps_sensor_report.py
@@ -9,7 +9,7 @@ from email.mime.text import MIMENonMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from datetime import datetime, timedelta
-RECEIVER_EMAILS="uuganbileg@tttools.mn,lhagvabayar.a@monospharmatrade.mn,undrakh.b@monospharmatrade.mn,anuerdene.b@monospharmatrade.mn,ayurzana.s@monospharmatrade.mn,munkhtamir.b@monospharmatrade.mn,baatarkhuu@monospharmatrade.mn,ariuntungalag@monospharmatrade.mn,narmandakh.b@monospharmatrade.mn"
+RECEIVER_EMAILS="dorjsuren.n@gmail.com,lhagvabayar.a@monospharmatrade.mn,undrakh.b@monospharmatrade.mn,anuerdene.b@monospharmatrade.mn,ayurzana.s@monospharmatrade.mn,munkhtamir.b@monospharmatrade.mn,baatarkhuu@monospharmatrade.mn,ariuntungalag@monospharmatrade.mn,narmandakh.b@monospharmatrade.mn"
 
 # Configuration (Replace with your actual details)
 CONFIG = {
